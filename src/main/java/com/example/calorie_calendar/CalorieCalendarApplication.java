89fe5a34bffa321d91cb9803c21ed7c67f3eae95
raananrglashofer <raananrg@gmail.com>
2025-01-07 16:06:17 -0500

    initial commit

---
+++ src/main/java/com/example/calorie_calendar/CalorieCalendarApplication.java
@@ -0,0 +1,13 @@
+package com.example.calorie_calendar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalorieCalendarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalorieCalendarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/calorie_calendar/CalorieCalendarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.calorie_calendar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalorieCalendarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

93605d505ab31b2331eeef77f23b4cd726729d05
raananrglashofer <raananrg@gmail.com>
2025-01-14 12:20:45 -0600

    added id to user class 1/14 12:20PM

--- src/main/java/com/example/calorie_calendar/calendar/User.java
+++ src/main/java/com/example/calorie_calendar/calendar/User.java
@@ -1,7 +1,15 @@
 package com.example.calorie_calendar.calendar;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotEmpty;
+@Entity
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
     private final String name;
     private double weight;
     private int height;
